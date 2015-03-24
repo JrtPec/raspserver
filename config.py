7d4e09511317c6807f115e5f240da2c891acfb76
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-03-24 14:43:33 +0100

    database support

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+
 SECRET_KEY = "secret_key"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
